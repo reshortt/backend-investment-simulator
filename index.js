e3c58bbe5aa9322cc2395a7b4f9ffa33c646a1db
rshortt <rshortt@xilinx.com>
2021-09-06 12:39:51 -0700

    print out local mongodb

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const {MongoClient}=require('mongodb')
+
+const url = 'mongodb://localhost:27017'
+const client = new MongoClient(url)
 
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send ("It works")
+router.get('/investors/fetch_all', async (req, res) => {
+
+    await client.connect();
+    const db = client.db('investments')
+    const collection = db.collection('assets')
+    const findResult = await (collection.find({})).toArray()
+
+
+    res.send (findResult)
     console.log (req)
 })
 
@@ -12,4 +23,6 @@ app.use('/', router);
 const server = app.listen(3005,  () => {
     console.log ("backend is running");
 });
+
+
 
