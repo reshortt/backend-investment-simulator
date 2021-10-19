7d3268bdc89b1f2178e55ed4a2ec1a07fcf4efa8
rshortt <rshortt@xilinx.com>
2021-10-19 00:35:07 -0700

    signup and login are supported from front and back end

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ import e = require("express");
 import yahooStockAPI = require("yahoo-stock-api");
 import yahooHistory = require("yahoo-finance-history");
 global.fetch = require("node-fetch");
+import bodyParser = require("body-parser");
 
 const url: string = "mongodb://localhost:27017";
 const client: MongoClient = new MongoClient(url);
@@ -13,15 +14,19 @@ const client: MongoClient = new MongoClient(url);
 const port: number = 3005;
 
 const router: express.Router = express.Router();
-router.get("/investors/fetch_all", async (req, res) => {
+router.post("/login", express.json(), async (req, res) => {
+  console.log(req.body);
   await client.connect();
   const db = client.db("investments");
-  const collection = db.collection("assets");
-  console.log("investors is " + collection);
-  const findResult = await collection.find({}).toArray();
-  console.log("req is " + req);
+  const collection = db.collection("investors");
+  const foundUser = await collection.findOne({
+    email: req.body.userId,
+    password: req.body.password,
+  });
 
-  res.send(findResult);
+  console.log(foundUser);
+
+  res.send(foundUser);
 });
 
 router.get("/assets/:tickerSymbol", async (req, res) => {
@@ -48,7 +53,7 @@ const server = app.listen(port, () => {
   console.log("backend is running");
 });
 
-async function isTickerSymbolValid(tickerSymbol: string) {
+async function checkFavoriteStock(tickerSymbol: string) {
   const data = await yahooHistory.getPriceHistory(tickerSymbol);
   console.log(await data.dividendHistory);
 
@@ -64,13 +69,21 @@ async function isTickerSymbolValid(tickerSymbol: string) {
 }
 
 router.post("/signup", express.json(), async (req, res) => {
+  console.log("ackend signup entered");
   const email = req.body.email;
   const password = req.body.password;
   const name = req.body.name;
 
   await client.connect();
+  console.log("client connect");
   const db = client.db("investments");
   const collection = db.collection("investors");
+  console.log("investors colection obtained");
+
+console.log("email is " + email)
+console.log("password is " + password)
+console.log("name is" + name)
+
 
   if (!email || !password) {
     res.status(400);
@@ -79,8 +92,8 @@ router.post("/signup", express.json(), async (req, res) => {
     });
     return;
   }
-
-  const isValid = await isTickerSymbolValid(password);
+  // console.log("checking MCK stock");
+  // const isValid =  checkFavoriteStock("MCK");
 
   // check for valid email string
   let doc = await collection.findOne({ email: email });
@@ -118,7 +131,7 @@ router.post("/signup", express.json(), async (req, res) => {
       $set: {
         cash: 100000,
       },
-    },
+    }
   );
 
   res.status(200);
